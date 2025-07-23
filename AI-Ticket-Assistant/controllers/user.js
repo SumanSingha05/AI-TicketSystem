82848243bb584a552f901aa2100aa05d64608990
SumanSingha05 <sumansingha8654@gmail.com>
2025-07-23 23:30:47 +0530

    Basic user controller initialized.

---
+++ AI-Ticket-Assistant/controllers/user.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/user.js"
+import { ingest } from "../inngest/client.js"
+
+export const signup = async (req, res) => {
+
+}
